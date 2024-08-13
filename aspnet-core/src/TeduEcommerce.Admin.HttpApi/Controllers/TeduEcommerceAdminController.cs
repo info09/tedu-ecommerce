095be3f86bbd423c45f2e3c79a7567d8514ae2fa
huytq@ics-p.vn <huytq@ics-p.vn>
2024-08-13 13:16:22 +0700

    Finish folder structure

---
+++ aspnet-core/src/TeduEcommerce.Admin.HttpApi/Controllers/TeduEcommerceAdminController.cs
@@ -0,0 +1,14 @@
+﻿using TeduEcommerce.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace TeduEcommerce.Admin.Controllers;
+
+/* Inherit your controllers from this class.
+ */
+public abstract class TeduEcommerceAdminController : AbpControllerBase
+{
+    protected TeduEcommerceAdminController()
+    {
+        LocalizationResource = typeof(TeduEcommerceResource);
+    }
+}
