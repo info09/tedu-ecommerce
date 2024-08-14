4f298970dde217cd83aa76a0174e3ea30ce2b203
huytq@ics-p.vn <huytq@ics-p.vn>
2024-08-14 11:08:42 +0700

    [FE] Hiển thị thông tin và đăng xuất

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { AuthService } from './shared/services/auth.service';
+import { Router } from '@angular/router';
+import { LOGIN_URL } from './shared/constants/urls.const';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +11,18 @@ import { PrimeNGConfig } from 'primeng/api';
 export class AppComponent implements OnInit {
   menuMode = 'static';
 
-  constructor(private primeNGConfig: PrimeNGConfig) {}
+  constructor(
+    private primeNGConfig: PrimeNGConfig,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.primeNGConfig.ripple = true;
     document.documentElement.style.fontSize = '14px';
+
+    if (this.authService.isAuthenticate() === false) {
+      this.router.navigate([LOGIN_URL]);
+    }
   }
 }
--- angular/src/app/shared/services/auth.service.ts
+++ angular/src/app/shared/services/auth.service.ts
@@ -4,6 +4,7 @@ import { LoginRequestDto } from '../models/login-request.dto';
 import { Observable } from 'rxjs';
 import { LoginResponseDto } from '../models/login-response.dto';
 import { environment } from 'src/environments/environment';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants/keys.const';
 
 @Injectable({
   providedIn: 'root',
@@ -28,5 +29,14 @@ export class AuthService {
       data,
       { headers: { 'Content-type': 'application/x-www-form-urlencoded' } }
     );
+  }
+
+  public logout() {
+    localStorage.removeItem(ACCESS_TOKEN);
+    localStorage.removeItem(REFRESH_TOKEN);
+  }
+
+  public isAuthenticate(): boolean {
+    return localStorage.getItem(ACCESS_TOKEN) != null;
   }
 }
